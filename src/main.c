6c629940909400b1d3fcbef24f53ff4c25a93580
ghkdxofla <ghkdxofla@gmail.com>
2023-09-29 14:24:03 +0900

    feat: add codes

--- src/main.c
+++ src/main.c
@@ -1,7 +1,20 @@
+#include <termios.h>
 #include <unistd.h>
 
+void enableRawMode() {
+    struct termios raw; // struct for storing terminal attributes
+
+    tcgetattr(STDIN_FILENO, &raw); // get terminal attributes
+
+    raw.c_lflag &= ~(ECHO); // disable echoing of input
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); // set terminal attributes
+}
+
 int main() {
+    enableRawMode();
+    
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1);
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
 }
