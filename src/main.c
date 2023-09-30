585b223035571cb48785737ce15fce986369037b
ghkdxofla <ghkdxofla@gmail.com>
2023-09-30 21:08:10 +0900

    feat: add init editor

--- src/main.c
+++ src/main.c
@@ -14,6 +14,8 @@
 /*** data ***/
 
 struct editorConfig {
+    int screenrows;
+    int screencols;
     struct termios orig_termios;
 };
 
@@ -228,8 +230,13 @@ void editorProcessKeypress() {
 
 /*** init ***/
 
+void initEditor() {
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
 int main() {
     enableRawMode();
+    initEditor();
 
     while (1) {
         editorRefreshScreen();
