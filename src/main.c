589370d97f910717c7059901470967e4ef33d8c0
ghkdxofla <ghkdxofla@gmail.com>
2023-09-30 21:04:30 +0900

    feat: get window size

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -142,6 +143,28 @@ char editorReadKey() {
     }
 
     return c;
+}
+
+/**
+ * getWindowSize()
+ * uses the TIOCGWINSZ ioctl to get the size of the terminal window.
+ * 
+ * `ioctl()`
+ * The ioctl() function manipulates the underlying device parameters of special files.
+ * 
+ * `TIOCGWINSZ`
+ * TIOCGWINSZ is an I/O control request that gets the window size of the terminal.
+*/
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) { // get window size
+        return -1;
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
 }
 
 /*** output ***/
