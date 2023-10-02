497c1c73cffe9e5c5a21a71053833e44568baa0f
ghkdxofla <ghkdxofla@gmail.com>
2023-10-02 18:53:51 +0900

    feat: moving left at the start of a line

--- src/main.c
+++ src/main.c
@@ -505,6 +505,10 @@ void editorMoveCursor(int key) {
     switch (key) {
         case ARROW_LEFT:
             if (E.cx != 0) E.cx--;
+            else if (E.cy > 0) {
+                E.cy--;
+                E.cx = E.row[E.cy].size;
+            }
             break;
         case ARROW_RIGHT:
             if (row && E.cx < row->size) E.cx++;
