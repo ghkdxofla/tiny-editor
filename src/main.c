aedfeb58dace25654b09a4bfac9dd60d22291bf3
ghkdxofla <ghkdxofla@gmail.com>
2023-10-02 18:48:52 +0900

    feat: change cursor to the end of line

--- src/main.c
+++ src/main.c
@@ -501,12 +501,13 @@ void editorRefreshScreen() {
 /*** input ***/
 
 void editorMoveCursor(int key) {
+    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy]; // get current row (if it exists
     switch (key) {
         case ARROW_LEFT:
             if (E.cx != 0) E.cx--;
             break;
         case ARROW_RIGHT:
-            E.cx++;
+            if (row && E.cx < row->size) E.cx++;
             break;
         case ARROW_UP:
             if (E.cy != 0) E.cy--;
