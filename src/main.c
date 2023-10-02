78763d7d8a55397ba06203b5974f83db3010dcf5
ghkdxofla <ghkdxofla@gmail.com>
2023-10-02 20:25:38 +0900

    feat: enhance end key

--- src/main.c
+++ src/main.c
@@ -602,7 +602,7 @@ void editorProcessKeypress() {
             break;
 
         case END_KEY:
-            E.cx = E.screencols - 1;
+            if (E.cy < E.numrows) E.cx = E.row[E.cy].size;
             break;
 
         case PAGE_UP:
@@ -614,7 +614,7 @@ void editorProcessKeypress() {
                     E.cy = E.rowoff + E.screenrows - 1;
                     if (E.cy > E.numrows) E.cy = E.numrows;
                 }
-                
+
                 int times = E.screenrows;
                 while (times--) editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
             }
