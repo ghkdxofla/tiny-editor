fe15120b0789955637a59acc797fd358c4c8776d
ghkdxofla <ghkdxofla@gmail.com>
2023-10-07 14:35:36 +0900

    feat: upgrade text viewer to text editor

--- src/main.c
+++ src/main.c
@@ -711,6 +711,10 @@ void editorProcessKeypress() {
         case ARROW_RIGHT:
             editorMoveCursor(c);
             break;
+        
+        default:
+            editorInsertChar(c);
+            break;
     }
 }
 
