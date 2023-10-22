2627fbd46d815ed612a140f8b67d03c40d501357
ghkdxofla <ghkdxofla@gmail.com>
2023-10-22 22:46:36 +0900

    feat: add arrows to move when searches the word

--- src/main.c
+++ src/main.c
@@ -607,16 +607,35 @@ void editorSave() {
 /*** find ***/
 
 void editorFindCallback(char *query, int key) {
+    static int last_match = -1;
+    static int direction = 1; // 1 for forward, -1 for backward
+
     if (key == '\r' || key == '\x1b') { // if enter or escape is pressed
+        last_match = -1;
+        direction = 1;
         return;
+    } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+        direction = 1;
+    } else if (key == ARROW_LEFT || key == ARROW_UP) {
+        direction = -1;
+    } else {
+        last_match = -1;
+        direction = 1;
     }
 
+    if (last_match == -1) direction = 1;
+    int current = last_match;
     int i;
     for (i = 0; i < E.numrows; i++) { // search each row
-        erow *row = &E.row[i];
+        current += direction;
+        if (current == -1) current = E.numrows - 1; // wrap around to bottom of file
+        else if (current == E.numrows) current = 0; // wrap around to top of file
+
+        erow *row = &E.row[current];
         char *match = strstr(row->render, query); // strstr() returns a pointer to the first occurrence of query in row->render, or NULL if no match is found
         if (match) {
-            E.cy = i;
+            last_match = current;
+            E.cy = current;
             E.cx = editorRowRxToCx(row, match - row->render); // set cursor position to beginning of match
             E.rowoff = E.numrows; // scroll to bottom of file
             break;
@@ -630,7 +649,7 @@ void editorFind() {
     int saved_coloff = E.coloff;
     int saved_rowoff = E.rowoff;
 
-    char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+    char *query = editorPrompt("Search: %s (Use ESC/Arrows/Enter)", editorFindCallback);
     
     if (query) {
         free(query);
