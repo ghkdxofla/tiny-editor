81bf3088b9df050b9a9818797c057d4e575e2dcd
ghkdxofla <ghkdxofla@gmail.com>
2024-01-28 10:37:49 +0900

    feat: add terminal to enable raw mode

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,18 @@
-use termion::raw::IntoRawMode;
-use std::io::{self, Write};
+use crossterm::terminal;
+use std::env;
+use std::io;
+use std::io::Read;
 
 fn main() {
-    let stdout = io::stdout().into_raw_mode().unwrap();
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        // TODO: Add editor to open file
+        terminal::enable_raw_mode().expect("enable_raw_mode error");
+        println!("open a file: {}", args[1]);
 
-    write!(stdout, "{}", termion::clear::All).unwrap();
+        let mut buf = [0; 1];
+        while io::stdin().read(&mut buf).expect("read error") == 1 && buf != [b'q'] {
+            println!("read: {:?}", buf);
+        }
+    }
 }
