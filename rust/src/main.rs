050712de4fc9b68bbb7bdd2545ba3bae01423f48
ghkdxofla <ghkdxofla@gmail.com>
2024-01-30 23:42:32 +0900

    feat: change main.rs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,13 +3,16 @@ mod editor;
 use std::env;
 use editor::Editor;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() > 1 {
         println!("open a file: {}", args[1]);
 
         let editor = Editor::new();
-        editor.run();
+        editor.run()
+    }
+    else {
+        Err(std::io::Error::new(std::io::ErrorKind::NotFound, "no file specified"))
     }
 }
